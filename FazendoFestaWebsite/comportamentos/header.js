71dd0b4d49bb149e1da0fa69c63171884d15366f
Naceja <71296941+MrNaceja@users.noreply.github.com>
2022-10-06 22:38:49 -0300

    Start

---
+++ FazendoFestaWebsite/comportamentos/header.js
@@ -0,0 +1,14 @@
+/**
+ * Controla a abertura e fechamento do menu mobile.
+ * 
+ * @param {HTMLButtonElement} oInstanceBtn 
+ */
+function controlaMenu(oInstanceBtn) {
+    var oMenu = document.querySelector('header .container nav');
+    if (oMenu.classList.contains('menu_aberto')) {
+        oMenu.classList.replace('menu_aberto','menu_fechado');
+    } else {
+        oMenu.classList.replace('menu_fechado','menu_aberto');
+    }
+    
+}
